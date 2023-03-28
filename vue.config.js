368dc268512f1471f257917022b316ef73c1909c
nodriza-io <jprieto@nodriza.io>
2023-03-28 09:47:06 -0500

    adding build scripts

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // use this option for production linking
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/4.0/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/app/' : '/',
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
